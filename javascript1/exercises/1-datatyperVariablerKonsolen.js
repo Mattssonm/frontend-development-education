3ae8c0415df40496ab4316bed526ab7150af53ce
Markus <markusmattsson90@gmail.com>
2017-09-11 17:29:49 +0200

    Add instruction file, add 1-datatyperVariablerKonsolen.js file

---
+++ javascript1/exercises/1-datatyperVariablerKonsolen.js
@@ -0,0 +1,3 @@
+//The following is the code I wrote during my education. For exercise instructions see instructions.txt (swedish). 
+//Do not run this file, copy + paste the code you want to try/check into a javascript environment. 
+
