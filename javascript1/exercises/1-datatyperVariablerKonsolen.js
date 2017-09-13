28657824d9af9dcb2a456b366542dbee74f257be
Markus <markusmattsson90@gmail.com>
2017-09-13 18:40:00 +0200

    remove 1-datatyperVariablerKonsolen.js

--- javascript1/exercises/1-datatyperVariablerKonsolen.js
+++
@@ -1,3 +0,0 @@
-//The following is the code I wrote during my education. For exercise instructions see instructions.txt (swedish). 
-//Do not run this file, copy + paste the code you want to try/check into a javascript environment. 
-
