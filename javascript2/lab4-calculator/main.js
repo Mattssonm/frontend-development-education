25f51dd874638ccf71419b8de7cf8d825445f5be
Markus <markusmattsson90@gmail.com>
2018-04-14 17:56:38 +0200

    Fix calc bugs, add history list

--- javascript2/lab4-calculator/main.js
+++ javascript2/lab4-calculator/main.js
@@ -3,36 +3,48 @@ window.addEventListener("load", event => {
     el: '#root',
     data: {
       lastEntry: 0,
-      entryHistory: 0,
-      entryNumbers: [],
-      entryOperators: [],
-      //entryArr: [],
+      entryHistory: "0",
+      entriesToCalc: [],
       savedCalcs: [],
-      operator: ""
+      operator: "",
+      showList: false
     },
     methods: {
       addEntry: function(event) {
         let entry = event.path[0].innerHTML;
         
+        if (this.entryHistory.includes("=")) {
+          this.entryHistory = this.lastEntry;
+        }
+        
         //if entry is an operator
         if (isNaN(entry)) {
-          //push the latest number typed in & and the operator
-          this.pushLastNumberEntry();
-          this.entryOperators.push(entry);
-        } // if (isNaN(entry))
+          //push the latest number typed in
+          this.entriesToCalc.push(Number(this.lastEntry));
+          //assign the operator to last entry
+          this.lastEntry = entry;
+          //push the operator
+          this.entriesToCalc.push(this.lastEntry);
+          
+          //if lastEntry is 0 or an operator, replace it
+        } else if (this.lastEntry == 0 || isNaN(this.lastEntry)) {
+          this.lastEntry = entry;
+
+          //else add it
+        } else {
+          this.lastEntry += entry;
+        }
         
-        this.lastEntry = entry;
-        //this.entryArr.push(entry);
-        //if entry is zero, remove the zero
+        //if entry is zero, replace it
         if (this.entryHistory == 0) {
           this.entryHistory = entry;
+          //else add it
         } else {
           this.entryHistory += entry;
         }
-
       },
       clearEntries: function() {
-        this.entryHistory = 0;
+        this.entryHistory = "0";
         this.lastEntry = 0;
         this.entryArr = [];
       },
@@ -41,98 +53,35 @@ window.addEventListener("load", event => {
         this.clearEntries();
         this.savedCalcs = [];
       },
+      
       calculateEntries: function() {
-        this.pushLastNumberEntry();
-        this.lastEntry = this.evaluate(this.entryNumbers[0], this.entryOperators[0], this.entryNumbers[1]);
-        
+        this.entriesToCalc.push(Number(this.lastEntry));
+        this.evaluate(this.entriesToCalc);
+        this.entriesToCalc = [];
+        this.savedCalcs.push(this.entryHistory);
+        this.showList = true;
       },
-      pushLastNumberEntry: function() {
-          let str = this.entryHistory;
-          
-          //decide where to begin the loop
-          let i = 1;
-          //if an operator is last char, do not include it in loop
-          if (!isNaN(str.charAt.length-1)) {
-            i = 2;
-          }
-          let startingNum;
-        
-          //decide where number to push starts
-          while (!isNaN(str.charAt(str.length-i))) {
-            startingNum = str.length-i;
-            console.log("WhiLE")
-            if (str.length - i == -1) {
-              startingNum = 0;
-              break;
-            }
-            i++;
-          }
-          
-          let tempNum ="";
-          console.log(startingNum);
-          //generate number
-          for (let i=startingNum; i < str.length-1; i++) {
-            tempNum += "" + str.charAt(i);
-            console.log("FOR")
-          }
-          
-          this.entryNumbers.push(tempNum);
-      },
-      /*calculateEntries: function() {
-        //loop the entryArr
-        let num1;
-        let num2;
-        let currentOp;
-        let totalSum;
-        this.entryArr.forEach( entry => {
-          //if the entry is an operator
-          if (isNaN(entry)) {
-            //if there is no saved operator
-            if (currentOp === undefined) {
-              currentOp = entry;
-            } else {
-              totalSum = this.evaluate(num1, currentOp, num2);
-              this.lastEntry = totalSum;
-              num1 = undefined;
-              num2 = undefined;
-              currentOp = entry;
-            }
-          }
-          
-          //else the entry is a number
-          else {
-            //if there are no saved numbers and no currentOp
-            if (num1 === undefined && currentOp === undefined) {
-            //save it to tempnum
-            num1 = entry;
-            
-            //else if there are a num1 but no currentOp
-            } else if (num1 !== undefined && currentOp === undefined) { 
-              num1 = "" + num1 + entry;
-            
-            //else if there are a num1 and a currentOp nut no num2
-            } else if (num1 !== undefined && currentOp !== undefined && num2 === undefined) {
-              num2 = entry
-            //else num1 & currentOp & num2 exists
-            } else {
-              //concatenate num2 with entry
-              num2 = "" + num2 + entry;
-            }
-          }
-        }); //forEach
-        console.log(num1, currentOp, num2, totalSum);
-      },*/
-      evaluate: function (num1, op, num2) {
-        let ops = {
+      
+      evaluate: function(arr) {
+          let result = 0;
+          let ops = {
           '+': (x, y) => x + y,
           '-': (x, y) => x - y,
           '/': (x, y) => x / y,
           '*': (x, y) => x * y
         }
-        console.log(this.entryNumbers);
-        console.log(this.entryOperators);
-        console.log(ops[op](Number(num1), Number(num2)));
-        return ops[op](num1, num2);
+        
+        if (arr.length == 1) {
+          return arr[0];
+        } else {
+          //result += ops[arr[1]](arr[0], arr[2]);
+          
+          for (let i = 1; i<arr.length; i+=2) {
+            result += ops[arr[i]](arr[i-1], arr[i+1])
+          }
+        }
+        this.lastEntry = result;
+        this.entryHistory += "=" + result;
       }
     } //methods
   }); //vm
